8f449e046fe5deed452c91f6d4b827d2e15714c6
julesburn <cmbenson88@gmail.com>
2019-08-05 14:17:15 -0500

    WIP adding collaborators resource

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,6 +4,7 @@ module.exports = {
         const staticRoutes = require("../routes/static");
         const userRoutes = require("../routes/users");
         const wikiRoutes = require("../routes/wikis");
+        const collaboratorRoutes = require("../routes/collaborators");
 
         if(process.env.NODE_ENV === "test") {
             const mockAuth = require("../../spec/support/mock-auth.js");
@@ -14,6 +15,7 @@ module.exports = {
         app.use(staticRoutes);
         app.use(userRoutes);
         app.use(wikiRoutes);
+        app.use(collaboratorRoutes);
 
     }
 };
--- src/db/models/wiki.js
+++ src/db/models/wiki.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
-      type: Sequelize.INTEGER
+      type: DataTypes.INTEGER
     },
 
     title: {
